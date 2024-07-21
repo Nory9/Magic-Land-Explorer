40aaa57396720e04b47252cff58425c8206f1653
Nory9 <noormisk94@gmail.com>
2024-07-21 20:43:15 +0300

    implementation finished

---
+++ Magic-Land-Explorer/Destination.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Magic_Land_Explorer
+{
+    public class Destination
+    {
+        //"name": "Space Mountain",
+        //"type": "Indoor Roller Coaster",
+        //"location": "Tomorrowland",
+        //"duration": "98 minutes",
+        //"description"
+        public string name { get; set; }
+        public string type { get; set; }
+
+        public  string location { get; set; }
+
+        public string description { get; set; }
+
+        public string duration { get; set; }
+
+        public int DurationInMinutes
+        {
+            get
+            {
+                if (duration == null) return 0;
+
+                // Use a regular expression to find the first integer in the string
+                var match = Regex.Match(duration, @"\d+");
+                if (match.Success && int.TryParse(match.Value, out int minutes))
+                {
+                    return minutes;
+                }
+                return 0; // or throw an exception if you prefer
+            }
+        }
+    }
+}
--- Magic-Land-Explorer/Program.cs
+++ Magic-Land-Explorer/Program.cs
@@ -1,10 +1,144 @@
-﻿namespace Magic_Land_Explorer
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+
+namespace Magic_Land_Explorer
 {
+
+    public delegate void simpleDelegate();
+    public delegate void durationDelegate(int index, bool dutation);
     internal class Program
     {
+       static string json = File.ReadAllText("data.json");
+      static  List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(json);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            magicLandFlow();
+        }
+
+
+        public static void magicLandFlow()
+        {
+
+            simpleDelegate filteredDes = new simpleDelegate(Less_100);
+            simpleDelegate orderedNames = new simpleDelegate(sortedDestunationNames);
+
+            durationDelegate longestDurationDelesgate = new durationDelegate(longestDuration);
+
+           // durationDelegate TopThreeDuration = new durationDelegate()
+
+
+            while (true)
+            {
+                Console.WriteLine("\n-------------------------------------------------");
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1 - Show filtered destinations (duration < 100 minutes)");
+                Console.WriteLine("2 - Show destination with the longest duration");
+                Console.WriteLine("3 - Sort destinations by name");
+                Console.WriteLine("4 - Show top 3 longest duration destinations");
+                Console.WriteLine("5 - Exit");
+                Console.WriteLine("-------------------------------------------------\n");
+
+                
+                string userInput= Console.ReadLine();
+
+                switch (userInput) {
+
+                    case "1":
+
+                       filteredDes();
+                        break; 
+
+                    case "2":
+
+                        longestDurationDelesgate(1, false);
+                        break;
+                    case "3":
+
+                        orderedNames();
+                        break;
+
+                    case "4":
+
+                        longestDurationDelesgate(3, true);
+                        break ;
+
+                    case "5":
+
+                        Environment.Exit(0);    
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input please try again");
+                        break;
+
+                }
+            }
+        }
+
+        public static void Less_100() {
+            //var less_100 = from category in categories
+            //               from des in category.Destinations
+            //               where des.DurationInMinutes < 100
+            //               select des.name;
+
+            var less_100 = categories.SelectMany(catogery => catogery.Destinations).
+                Where(des => des.DurationInMinutes > 100).
+                Select(name => name.name);
+
+            foreach (var v in less_100)
+                Console.WriteLine(v);
+        }
+
+        public static void longestDuration(int index, bool duration) {
+
+            //var longestD = from category in categories
+            //               from des in category.Destinations
+            //               orderby des.DurationInMinutes descending
+            //               select new
+            //               {
+            //                   des.name,
+            //                   dd = duration ? $"{des.duration}" : ""
+
+            //               };
+
+            var longestD = categories.SelectMany(catogery => catogery.Destinations).
+                OrderByDescending(des => des.DurationInMinutes).
+                Select(des => new
+                {
+                    des.name,
+                    dd=duration ? $"{des.duration}" : ""
+                });
+
+            var longestDuration = longestD.ToList();          
+
+            for (int i = 0; i < index; i++)
+            {
+                if (duration)
+                    Console.WriteLine($"{longestDuration[i].name} - {longestDuration[i].dd}");
+                else
+                    Console.WriteLine(longestDuration[i].name);
+            }
+         
+            
+        
+        }
+
+        public static void sortedDestunationNames() {
+
+            //var sortedNames = from catogery in categories
+            //                  from des in catogery.Destinations
+            //                  orderby des.name 
+            //                  select des.name;
+
+            var sortedNmaes = categories.SelectMany(catogery => catogery.Destinations).
+                OrderBy(des=>des.name).
+                Select(des=>des.name).ToList();
+        
+          //  List<string> names=sortedNames.ToList();    
+
+            foreach (var name in sortedNmaes)
+                Console.WriteLine(name);
         }
     }
 }
